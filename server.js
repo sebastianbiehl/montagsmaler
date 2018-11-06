af62c3fca08fe54d40d3c4eebf498ec87a8407a3
Sebastian Biehl <sebastian.biehl@iem.thm.de>
2018-11-06 17:20:56 +0100

    Server Base Path changed

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const server = http.createServer(app);
 // This creates our socket using the instance of the server
 const io = socketIO(server);
 
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
 // This is what the socket.io syntax is like, we will work this later
 io.on("connection", socket => {
   console.log("User connected");
